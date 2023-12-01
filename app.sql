c669c6a21e8c5f971056a35d959d08aa1166360a
NadifaPermata <129677228+NadifaPermata@users.noreply.github.com>
2023-12-01 08:43:54 +0000

    update app

--- app.sql
+++ app.sql
@@ -14,5 +14,7 @@ insert into selling (nama_petugas, plat_nomor, jenis_kendaraan, bbm, banyak_pemb
 values
 	('Nuryanto', 'L 0919 CGF', 'mobil','pertamax turbo', 5, '08:00', '2023-10-01'),
 	('Angel', 'M 2348 GHT', 'motor', 'pertamax', 3, '09:00', '2022-10-02'),
-	('Angel', 'N 1087 AE', 'motor', 'pertamax', 2, '20:00', '2021-12-12')
+	('Angel', 'N 1087 AE', 'motor', 'pertamax', 2, '20:00', '2021-12-12'),
+	('Nuryanto', 'AE 2047 E', 'motor', 'pertamax', 3, '20:00', '2022-02-12')
+	('Angel', 'AG 0887 A', 'truk', 'solar', 10, '18:00', '2021-12-24')
 	;
